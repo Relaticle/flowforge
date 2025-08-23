b3130b62cf712d0cc94adea99becb646c8236035
manukminasyan <manuk.minasyan1@gmail.com>
2025-08-23 22:27:13 +0400

    Add exception classes for rank validation

---
+++ src/Exceptions/InvalidChars.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Relaticle\Flowforge\Exceptions;
+
+use InvalidArgumentException;
+use Throwable;
+
+use function implode;
+
+class InvalidChars extends InvalidArgumentException
+{
+    private function __construct(string $message = '', int $code = 0, Throwable|null $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+
+    /**
+     * @param non-empty-string       $rank
+     * @param non-empty-list<string> $chars
+     */
+    public static function forInputRankWithInvalidChars(string $rank, array $chars): self
+    {
+        return new self('Rank provided contains an invalid Char. Rank Provided: ' . $rank . ' - Invalid char: ' . implode(', ', $chars));
+    }
+}
---
+++ src/Exceptions/LastCharCantBeEqualToMinChar.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Relaticle\Flowforge\Exceptions;
+
+use InvalidArgumentException;
+use Throwable;
+
+class LastCharCantBeEqualToMinChar extends InvalidArgumentException
+{
+    private function __construct(string $message = '', int $code = 0, Throwable|null $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+
+    /**
+     * @param non-empty-string       $rank
+     * @param non-empty-list<string> $chars
+     */
+    public static function forRank(string $rank, string $minChar): self
+    {
+        return new self('The last char of the rank (' . $rank . ') can\'t be equal to the min char (' . $minChar . ').');
+    }
+}
