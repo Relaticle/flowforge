422745b8a91f9a99f295900a711a4731980a87fd
Mustafa Online <sawdjg@gmail.com>
2025-08-24 06:34:37 -0700

    Use logical properties on column

--- resources/views/livewire/column.blade.php
+++ resources/views/livewire/column.blade.php
@@ -9,7 +9,7 @@
                 {{ $column['label'] }}
             </h3>
             <div @class([
-                'ml-2 inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium',
+                'ms-2 inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium',
                 'bg-gray-100 text-gray-800 border border-gray-200 hover:bg-gray-200 dark:bg-gray-700/60 dark:text-gray-200 dark:border-gray-600 dark:hover:bg-gray-700' => ($column['color'] ?? 'default') === 'gray' || ($column['color'] ?? 'default') === 'default',
                 'bg-blue-50 text-blue-800 border border-blue-200 hover:bg-blue-100 dark:bg-blue-900/40 dark:text-blue-200 dark:border-blue-800/60 dark:hover:bg-blue-900/60' => ($column['color'] ?? 'default') === 'blue',
                 'bg-green-50 text-green-800 border border-green-200 hover:bg-green-100 dark:bg-green-900/40 dark:text-green-200 dark:border-green-800/60 dark:hover:bg-green-900/60' => ($column['color'] ?? 'default') === 'green',
