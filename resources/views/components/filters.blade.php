f328ff54f7a374f2f775893883b02d04ced852da
manukminasyan <manuk.minasyan1@gmail.com>
2025-08-25 20:41:55 +0400

    Refactor filters.blade.php for improved readability and maintainability

--- resources/views/components/filters.blade.php
+++ resources/views/components/filters.blade.php
@@ -1,22 +1,28 @@
 @php
-    use Illuminate\View\ComponentAttributeBag;use function Filament\Support\prepare_inherited_attributes;
-    $isFilterable = $this->getTable()->isFilterable();
-    $isFiltered = $this->getTable()->isFiltered();
-    $isSearchable = $this->getTable()->isSearchable();
+    use Filament\Support\Enums\IconSize;use Filament\Support\Icons\Heroicon;use Filament\Tables\Filters\Indicator;use Filament\Tables\View\TablesIconAlias;use Illuminate\View\ComponentAttributeBag;
+    use Filament\Support\Facades\FilamentView;
+    use Filament\Tables\View\TablesRenderHook;
+
+    use function Filament\Support\generate_icon_html;use function Filament\Support\prepare_inherited_attributes;
+    $table = $this->getTable();
+    $isFilterable = $table->isFilterable();
+    $isFiltered = $table->isFiltered();
+    $isSearchable = $table->isSearchable();
+    $filterIndicators = $table->getFilterIndicators();
 @endphp
 
 
 <div class="fi-ta-header-toolbar mb-4 ms-2">
-    <div class="fi-ta-actions fi-align-start fi-wrapped space-x-4 mb-2">
+    <div class="fi-ta-actions fi-align-start fi-wrapped space-x-4">
         @if($isFilterable)
             <x-filament::dropdown
                 placement="bottom-start"
-                :width="$this->getTable()->getFiltersFormWidth()"
-                :max-height="$this->getTable()->getFiltersFormMaxHeight()"
+                :width="$table->getFiltersFormWidth()"
+                :max-height="$table->getFiltersFormMaxHeight()"
                 class="fi-ta-filters-dropdown z-40"
             >
                 <x-slot name="trigger">
-                    {{ $this->getTable()->getFiltersTriggerAction()->badge($this->getTable()->getActiveFiltersCount()) }}
+                    {{ $table->getFiltersTriggerAction()->badge($table->getActiveFiltersCount()) }}
                 </x-slot>
 
                 <div class="fi-ta-filters-dropdown-panel" style="padding: calc(var(--spacing) * 6); ">
@@ -47,9 +53,9 @@
                     {{ $this->getTableFiltersForm()  }}
 
 
-                    @if ($this->getTable()->getFiltersApplyAction()->isVisible())
+                    @if ($table->getFiltersApplyAction()->isVisible())
                         <div class="fi-ta-filters-apply-action-ctn" style="padding-top: calc(var(--spacing) * 4)">
-                            {{ $this->getTable()->getFiltersApplyAction() }}
+                            {{ $table->getFiltersApplyAction() }}
                         </div>
                     @endif
                 </div>
@@ -60,20 +66,66 @@
         @if($isSearchable)
             {{-- Search input --}}
             <x-filament-tables::search-field
-                :debounce="$this->getTable()->getSearchDebounce()"
-                :on-blur="$this->getTable()->isSearchOnBlur()"
-                :placeholder="$this->getTable()->getSearchPlaceholder()"
+                :debounce="$table->getSearchDebounce()"
+                :on-blur="$table->isSearchOnBlur()"
+                :placeholder="$table->getSearchPlaceholder()"
             />
         @endif
     </div>
 
-    @if($isFiltered)
-        <div class="fi-ta-filter-indicators flex gap-1">
-            @foreach($this->getTable()->getFilterIndicators() as $indicator)
-                <x-filament::badge color="primary" size="sm">
-                    {{ $indicator->getLabel() }}
-                </x-filament::badge>
-            @endforeach
-        </div>
+    @if ($filterIndicators)
+        @if (filled($filterIndicatorsView = FilamentView::renderHook(TablesRenderHook::FILTER_INDICATORS, scopes: static::class, data: ['filterIndicators' => $filterIndicators])))
+            {{ $filterIndicatorsView }}
+        @else
+            <div class="fi-ta-filter-indicators">
+                <div>
+                        <span class="fi-ta-filter-indicators-label">
+                            {{ __('filament-tables::table.filters.indicator') }}
+                        </span>
+
+                    <div class="fi-ta-filter-indicators-badges-ctn">
+                        @foreach ($filterIndicators as $indicator)
+                            @php
+                                $indicatorColor = $indicator->getColor();
+                            @endphp
+
+                            <x-filament::badge :color="$indicatorColor">
+                                {{ $indicator->getLabel() }}
+
+                                @if ($indicator->isRemovable())
+                                    @php
+                                        $indicatorRemoveLivewireClickHandler = $indicator->getRemoveLivewireClickHandler();
+                                    @endphp
+
+                                    <x-slot
+                                        name="deleteButton"
+                                        :label="__('filament-tables::table.filters.actions.remove.label')"
+                                        :wire:click="$indicatorRemoveLivewireClickHandler"
+                                        wire:loading.attr="disabled"
+                                        wire:target="removeTableFilter"
+                                    ></x-slot>
+                                @endif
+                            </x-filament::badge>
+                        @endforeach
+                    </div>
+                </div>
+
+                @if (collect($filterIndicators)->contains(fn (Indicator $indicator): bool => $indicator->isRemovable()))
+                    <button
+                        type="button"
+                        x-tooltip="{
+                                content: @js(__('filament-tables::table.filters.actions.remove_all.tooltip')),
+                                theme: $store.theme,
+                            }"
+                        wire:click="removeTableFilters"
+                        wire:loading.attr="disabled"
+                        wire:target="removeTableFilters,removeTableFilter"
+                        class="fi-icon-btn fi-size-sm"
+                    >
+                        {{ generate_icon_html(Heroicon::XMark, alias: TablesIconAlias::FILTERS_REMOVE_ALL_BUTTON, size: IconSize::Small) }}
+                    </button>
+                @endif
+            </div>
+        @endif
     @endif
 </div>
