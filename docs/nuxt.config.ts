109e0607db8e7a421e6631d5a6d8526c919db457
manukminasyan <manuk.minasyan1@gmail.com>
2025-09-11 22:17:42 +0400

    Configure image provider in Nuxt settings to 'none'

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -6,6 +6,9 @@ export default defineNuxtConfig({
     app: {
         baseURL: process.env.NODE_ENV === 'production' ? '/flowforge/' : '/'
     },
+    image: {
+        provider: 'none'
+    },
     content: {
         build: {
             markdown: {
