dcde8fd2fb32a5deeca358a2ed39b3f92c815c2f
Hugh Messenger <hugh.messenger@gmail.com>
2026-04-20 14:08:16 -0500

    Move convertEnumToString method to helper, fix rebalancing to use it, add enums to test

---
+++ tests/Unit/EnumHelperTest.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+use Relaticle\Flowforge\Support\EnumHelper;
+use Relaticle\Flowforge\Tests\Fixtures\StatusEnum;
+
+describe('EnumHelper::convertEnumToString()', function () {
+    test('returns string value of a backed enum', function () {
+        expect(EnumHelper::convertEnumToString(StatusEnum::ToDo))->toBe('todo')
+            ->and(EnumHelper::convertEnumToString(StatusEnum::InProgress))->toBe('in_progress');
+    });
+
+    test('returns string unchanged', function () {
+        expect(EnumHelper::convertEnumToString('todo'))->toBe('todo')
+            ->and(EnumHelper::convertEnumToString('anything'))->toBe('anything');
+    });
+
+    test('casts integer to string', function () {
+        expect(EnumHelper::convertEnumToString(42))->toBe('42');
+    });
+
+    test('falls back to class basename for unknown objects', function () {
+        $obj = new class {};
+
+        expect(EnumHelper::convertEnumToString($obj))->toBeString()->not->toBeEmpty();
+    });
+
+    test('returns value from object with value property', function () {
+        $obj = new class
+        {
+            public string $value = 'from_value_property';
+        };
+
+        expect(EnumHelper::convertEnumToString($obj))->toBe('from_value_property');
+    });
+
+    test('returns result of __toString for stringable objects', function () {
+        $obj = new class
+        {
+            public function __toString(): string
+            {
+                return 'stringable_result';
+            }
+        };
+
+        expect(EnumHelper::convertEnumToString($obj))->toBe('stringable_result');
+    });
+});
